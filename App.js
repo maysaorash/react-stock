33c59622e68184ebe90c5bc4b1b304c69e57e142
maysaorash <72366798+maysaorash@users.noreply.github.com>
2021-04-16 00:19:16 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
